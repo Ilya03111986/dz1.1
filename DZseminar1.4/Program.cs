e7a954719b48ae97ac3e35dd5713471fec68a9d8
Ilya Perederey <ilya_soldat@mail.ru>
2022-12-03 20:00:35 +0300

    DZ1.4

---
+++ DZseminar1.4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int until = int.Parse(Console.ReadLine());
+int i = 0;
+int num = 0;
+while (i<until)
+
+{   num = num +2;
+    Console.WriteLine("{0}", num );
+    i=i+2;
+}
+
