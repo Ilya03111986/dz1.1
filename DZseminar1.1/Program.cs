fe321a8b2bf59db157a112d92107cdafe635778b
Ilya Perederey <ilya_soldat@mail.ru>
2022-12-03 16:16:00 +0300

    DZseminar1.1

---
+++ DZseminar1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число!");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число!");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1>n2)
+{ 
+    Console.WriteLine( "max: " +n1 );
+    Console.WriteLine( "min: " +n2 );
+}    
+    if (n1==n2)
+
+    {
+        Console.WriteLine( "числа равны" );
+
+    }
+
+
+   
+  
